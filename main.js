40a56c7dd28cb3b7a0b3c1bb0c80e5178a0a0a1c
abrilsole <absole@itba.edu.ar>
2023-11-13 20:34:58 -0300

    f

--- main.js
+++ main.js
@@ -1,27 +1,27 @@
-// function cantrestantes() {
-//     let cantRusuario;
+function cantrestantes() {
+    let cantRusuario;
 
-//     do {
-//         cantRusuario = prompt('Ingrese la cantidad de recetas que pidió este mes');
+    do {
+        cantRusuario = prompt('Ingrese la cantidad de recetas que pidió este mes');
 
-//         // Verificar si es un número válido
-//         if (isNaN(cantRusuario)) {
-//             alert('Por favor, ingrese un número válido.');
-//         } else {
-//             // Convertir la entrada a un número
-//             cantRusuario = parseInt(cantRusuario);
+        // Verificar si es un número válido
+        if (isNaN(cantRusuario)) {
+            alert('Por favor, ingrese un número válido.');
+        } else {
+            // Convertir la entrada a un número
+            cantRusuario = parseInt(cantRusuario);
 
-//             // Verificar si está en el rango deseado
-//             if (cantRusuario > 0 && cantRusuario < 10) {
-//                 let rrestantes = 10 - cantRusuario;
-//                 alert(`Le quedan ${rrestantes} recetas`);
-//                 break; // Salir del bucle si la entrada es válida
-//             } else {
-//                 alert('Por favor, ingrese un número mayor a 0 y menor a 10.');
-//             }
-//         }
-//     } while (true); // Bucle infinito hasta que se ingrese una entrada válida
-// }
+            // Verificar si está en el rango deseado
+            if (cantRusuario > 0 && cantRusuario < 10) {
+                let rrestantes = 10 - cantRusuario;
+                alert(`Le quedan ${rrestantes} recetas`);
+                break; // Salir del bucle si la entrada es válida
+            } else {
+                alert('Por favor, ingrese un número mayor a 0 y menor a 10.');
+            }
+        }
+    } while (true); // Bucle infinito hasta que se ingrese una entrada válida
+}
 
 
 
